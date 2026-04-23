6dbeb115b57970b01f028b2b82448ee550325e3b
Shib <shibjyoti252@gmail.com>
2026-04-24 02:20:20 +0530

    V1.1.3

--- Backend/app/database.py
+++ Backend/app/database.py
@@ -8,7 +8,10 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"ssl": {"ssl_mode": "REQUIRED"}}
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
