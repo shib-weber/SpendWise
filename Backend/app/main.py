95c740e9fd40ee27e02f2aa6559f89a2ba4f882c
Shib <shibjyoti252@gmail.com>
2026-05-02 19:52:00 +0530

    V1.2.15

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -23,7 +23,7 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 1440
 
 # Email Settings from .env
 SMTP_SERVER = os.getenv("SMTP_SERVER")
-SMTP_PORT = int(os.getenv("SMTP_PORT", 465))
+SMTP_PORT = int(os.getenv("SMTP_PORT", 587))
 SMTP_USERNAME = os.getenv("SMTP_USERNAME")
 SMTP_PASSWORD = os.getenv("SMTP_PASSWORD")
 
